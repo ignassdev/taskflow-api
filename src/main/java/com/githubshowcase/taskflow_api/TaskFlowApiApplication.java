946d738939674d52c3ec95332e5de9ca44655462
Ignas Sungaila <ignas.s.dev@gmail.com>
2025-10-22 23:47:25 +0300

    chore: spring initial setup

---
+++ src/main/java/com/githubshowcase/taskflow_api/TaskFlowApiApplication.java
@@ -0,0 +1,13 @@
+package com.githubshowcase.taskflow_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskFlowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskFlowApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/githubshowcase/taskflow_api/TaskFlowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githubshowcase.taskflow_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskFlowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
